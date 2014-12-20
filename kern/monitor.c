f83c9a6a228a6c63e7d3cc1e5f40947538e54acf
yubo <yubo@xiaomi.com>
2014-12-20 00:14:16 -0800

    lab1 done
    Signed-off-by: yubo <yubo@xiaomi.com>

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display information about the kernel", mon_backtrace},
 };
 #define NCOMMANDS (sizeof(commands)/sizeof(commands[0]))
 
@@ -58,7 +59,24 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
+	uint32_t *ebp;
+	struct Eipdebuginfo info;
+	cprintf("Stack backtrac:\n");
+
+	ebp = (uint32_t *)read_ebp();
+
+	while(ebp){
+		cprintf("ebp %08x eip %08x args %08x %08x %08x %08x %08x\n", 
+				&ebp[0], ebp[1], ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+		debuginfo_eip(ebp[1], &info);
+			cprintf("\t%s:%u: %.*s+%u\n", 
+				info.eip_file,
+				info.eip_line,
+				info.eip_fn_namelen,
+				info.eip_fn_name,
+				info.eip_fn_addr);
+		ebp = (uint32_t *)ebp[0];
+	}
 	return 0;
 }
 
